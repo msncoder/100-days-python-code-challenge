89e38cb0da226549ea6f91e759d890eabc97c1e5
saad <msncoder1999@gmail.com>
2025-07-24 16:56:09 +0500

    6 day half complete

--- day5.py
+++ day5.py
@@ -127,11 +127,11 @@
 
 # Fibonacci  using recursion 
 
-def Fibonacci(n):
-    if n == 0 or n == 1:
-        return 1
+# def Fibonacci(n):
+#     if n == 0 or n == 1:
+#         return 1
     
-    return Fibonacci(n-1) + Fibonacci(n-2)
+#     return Fibonacci(n-1) + Fibonacci(n-2)
 
-for i in range(10):
-    print(Fibonacci(i), end=" ")
+# for i in range(10):
+#     print(Fibonacci(i), end=" ")
