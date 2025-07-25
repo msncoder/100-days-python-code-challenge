faf4c66a051748f3f03ec5d6dc23583e65018a48
Nazia <1455490aptechiic@gmail.com>
2025-07-25 18:27:30 +0500

    day7

---
+++ day7.py
@@ -0,0 +1,29 @@
+# Create a dictionary of 5 students with names as keys and marks as values
+
+# dic = {"Ali": 87, "Sara": 92,"bilal":99,"samad":85,"fahad":55}
+
+# # print(dic.keys())
+# # print(f"{dic.keys()} has got {dic.values()} Marks")
+
+# for i in dic.items():
+#     print(f"{i[0]} has got {i[1]} Marks")
+
+# Take a name input from user and check if student exists
+# → if yes, print marks.
+
+# name = input("Enter a Name")
+# dic = {"Ali": 87, "Sara": 92,"bilal":99,"samad":85,"fahad":55}
+
+
+# if name in dic.keys():
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# Find the student with highest marks
+
+dic = {"Ali": 87, "Sara": 92,"bilal":99,"samad":85,"fahad":55}
+
+print(dic.values())
+
