180dabc56b6c46fc734b2fe1c370fd4a27d8f360
saad <msncoder1999@gmail.com>
2025-07-26 04:05:26 -0700

    day7 done

--- day7.py
+++ day7.py
@@ -23,7 +23,72 @@
 
 # Find the student with highest marks
 
-dic = {"Ali": 87, "Sara": 92,"bilal":99,"samad":85,"fahad":55}
+# dic = {"Ali": 87, "Sara": 92,"bilal":99,"samad":85,"fahad":55}
+# student_name = ''
+# highest_marks = -1
 
-print(dic.values())
+# for name,marks in dic.items():
+#     if marks > highest_marks:
+#         highest_marks = marks
+#         student_name = name
+
+# print(f"The name of student is {student_name} has highest marks {highest_marks}")
+
+
+# Count frequency of characters in a string using dict
+
+# Input: "banana" → Output: {'b': 1, 'a': 3, 'n': 2}
+
+# inp = input("Enter a word")
+# count = {}
+
+# for i in inp:
+#     if i in count:
+#         count[i] += 1
+#     else:
+#         count[i] = 1
+# print(count)
+
+# Convert this dictionary to a JSON string and print:
+
+import json
+
+# data = {
+#     "name":"saad",
+#     "age":15,
+#     "skills":["Python","Django"]
+# }
+
+# json_string = json.dumps(data,indent=1)
+# print(type(json_string)) 
+
+
+# data = {
+#     "name": "Saad",
+#     "age": 15,
+#     "skills": ["Python", "Django"]
+# }
+
+# with open("data.json","w") as f:
+#     json.dump(data,f,indent=4)
+
+
+# Load the file and print content
+
+# with open("data.json","r") as f:
+#     loaded_data = json.load(f)
+#     print(loaded_data)
+
+
+# Write user input to notes.txt
+
+# inp = input("Enter a note")
+
+# with open("notes.txt",'w') as f:
+#     f.write(inp)
+
+
+# with open('notes.txt','r') as f:
+#     content = f.read()
+#     print(content)
 
